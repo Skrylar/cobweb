0a13be10a4b408080453581ae296e85509e453b9
Joshua Cearley <joshua.cearley@gmail.com>
2017-11-20 03:50:58 -0600

    Run touch procs when assigning new values.

--- cobweb.nim
+++ cobweb.nim
@@ -15,6 +15,7 @@ proc make_cobweb*(): Cobweb =
   newseq(result.values, 0)
 
 proc intern(self: var Cobweb; name: string): int =
+  ## Put in a name, get an identifier.  If the name is not known, create an identifier and return that.
   if self.intern_table.haskey(name):
     return self.intern_table[name]
   else:
@@ -22,16 +23,20 @@ proc intern(self: var Cobweb; name: string): int =
     self.intern_table[name] = result
     self.values.add(CobwebBinding(value: nil))
 
-proc touch*(self: Cobweb; name: string) =
-  ## Acts as though a variable has been changed, without actually changing it.  Used for complex situations.
+proc touch(self: var Cobweb; name: int) =
   discard
+
+proc touch*(self: var Cobweb; name: string) =
+  ## Acts as though a variable has been changed, without actually changing it.  Used for complex situations.
+  if self.intern_table.haskey(name):
+    self.touch self.intern_table[name]
 
 proc `[]=`*(self: var Cobweb; name: string; value: JsonNode) =
   ## Sets a variable in the cobweb.  Notifies the dependency chain within the cobweb of a change.
 
-  self.values[self.intern(name)].value = value
-
-  # TODO update chain
+  let id = self.intern(name)
+  self.values[id].value = value
+  self.touch id
 
 proc `[]`*(self: Cobweb; name: string): JsonNode =
   ## Retrieves a variable from the cobweb.
